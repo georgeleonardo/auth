a8ce6a5d438d640022b6406f44ef5c2e2c730204
George Leonardo <george.leonardo@booking.com>
2017-01-30 13:29:15 +0100

    starting login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginFrom extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
@@ -11,14 +11,23 @@ class LoginFrom extends Component {
           <Input
             placeholder="your@email.com"
             label="Email"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text: text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
 
         <CardSection>
-          <Button>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
